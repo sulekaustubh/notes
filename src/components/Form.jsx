5116faa9a2a31c694723ccde4e71be13a80c6142
sulekaustubh <sulekaustubh97@gmail.com>
2023-05-08 04:04:25 +0530

    tap and click features

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -23,15 +23,35 @@ const Form = ({ inputText, setInputText, todos, setTodos }) => {
                 type="text"
                 placeholder="Write here.."
             />
-            <div className="pt-2 ">
+            <div className="pt-2 flex place-content-between">
                 <button
                     // className="bg-blue-500 text-4xl w-10 text-slate-50 font-semibold rounded-full tracking-wider font-Quicksand"
-                    className={`${inputText.length === 0 ? 'pointer-events-none select-none bg-blue-400 text-4xl w-10 text-slate-50 font-semibold rounded-full tracking-wider font-Quicksand' : 'bg-blue-500  select-none text-4xl w-10 text-slate-50 font-semibold rounded-full tracking-wider font-Quicksand'}`}
+                    className={`${
+                        inputText.length === 0
+                            ? 'pointer-events-none focus:outline-none select-none bg-blue-400 text-4xl w-10 text-slate-50 font-semibold rounded-full tracking-wider font-Quicksand'
+                            : 'bg-blue-500 focus:outline-none select-none text-4xl w-10 text-slate-50 font-semibold rounded-full tracking-wider font-Quicksand'
+                    }`}
                     onClick={submitTodoHandler}
                     type="submit"
                 >
                     +
                 </button>
+                <div className="xs:text-[10px] tracking-wide border-black md:text-xs pr-1 text-slate-500 select-none">
+                    <div className='absolute'>
+                        <div className="invisible lg:visible">
+                            *single click - task done
+                        </div>
+                        <div className="invisible lg:visible">
+                            *double click - task done
+                        </div>
+                    </div>
+                    <div className="">
+                        <div className="lg:invisible">*single tap - task done</div>
+                        <div className="lg:invisible ">
+                            *double tap - task delete
+                        </div>
+                    </div>
+                </div>
             </div>
         </form>
     );
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -19,8 +19,12 @@ const Todo = ({ text, todo, todos, setTodos }) => {
         );
     };
     return (
-        <div className="font-Quicksand flex font-semibold">
-            <div className="my-2 space-x-2 flex border-[1.2px] border-t-blue-900 border-b-blue-900 border-r-blue-900 bg-gradient-to-r from-slate-50 to-orange-100 rounded-r-full">
+        <div className="font-Quicksand flex pr-2 font-semibold">
+            <div
+                onClick={completeHandler}
+                onDoubleClick={deleteHandler}
+                className="my-2 cursor-pointer focus:outline-none space-x-2 flex border-[1.2px] border-t-blue-900 border-b-blue-900 border-r-blue-900 bg-gradient-to-r from-slate-50 to-orange-100 rounded-r-full"
+            >
                 <div className="">
                     {/* <input type="text" value={text} /> */}
                     {/* <textarea
@@ -35,15 +39,15 @@ const Todo = ({ text, todo, todos, setTodos }) => {
                         className={` ${
                             todo.completed
                                 ? 'w-auto break-all duration-200 line-through select-none py-4 tracking-widest pr-6 px-4 bg-slate-800/0 text-slate-500/40 '
-                                // : 'w-auto select-none py-4 overflow-scroll hidebar tracking-widest px-4 pr-6 bg-slate-800/0 text-slate-600'
-                                : 'w-auto break-all select-none py-4 hidebar tracking-widest px-4 pr-6 bg-slate-800/0 text-slate-600'
+                                : // : 'w-auto select-none py-4 overflow-scroll hidebar tracking-widest px-4 pr-6 bg-slate-800/0 text-slate-600'
+                                  'w-auto break-all select-none py-4 hidebar tracking-widest px-4 pr-6 bg-slate-800/0 text-slate-600'
                         }`}
                     >
                         {text}
                     </div>
                 </div>
             </div>
-            <div className="center-flex pt-1 w-[10%] lg:w-[5%]">
+            {/* <div className="center-flex pt-1 w-[10%] lg:w-[5%]">
                 <div className="">
                     <button
                         className='bg-[url("https://raw.githubusercontent.com/sulekaustubh/image-vault/main/done.jpg")] h-6 w-6 bg-cover bg-center'
@@ -56,7 +60,7 @@ const Todo = ({ text, todo, todos, setTodos }) => {
                         onClick={deleteHandler}
                     ></button>
                 </div>
-            </div>
+            </div> */}
         </div>
     );
 };
