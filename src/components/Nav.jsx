b0ccaf57cf666f8fd36d7f426d10b8461c9ec59a
sulekaustubh <sulekaustubh97@gmail.com>
2023-05-08 02:54:42 +0530

    Revert "test"
    This reverts commit d8d90c18b9b1e55f62bb453b2c459d841d8414bd.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ const Nav = () => {
             <div className="bg-slate-800 flex justify-center select-none rounded-b-3xl pt-2 text-slate-50 text-3xl font-JoseFinSans tracking-[0.2em]">
                 <div>N</div>
                 <div className='bg-[url("https://raw.githubusercontent.com/sulekaustubh/image-vault/main/o.png")] animate-spin animate-infinite animate-duration-[16000ms] animate-normal ml-[1px] mr-1 mt-[3px] h-6 w-6 bg-cover bg-center'></div>
-                <div>TES..</div>
+                <div>TES</div>
             </div>
         </div>
     );
